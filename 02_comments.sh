1033a257553f202ad15ea866fff1bf520ec0de72
Prajjawal <prajjawaldy.zhcet@gmail.com>
2023-08-20 16:22:50 +0530

    day1

---
+++ 02_comments.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "This is a comment script"
+#This is a single line comment
+<<This 
+is 
+a 
+Multi Line 
+Comment
+This
+
---
+++ 03_variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Script to show how to use variables
+a=10
+name="Prajjawal"
+age=21
+echo "My name is $name and my age is $age"
+#var to store the output of the command
+HOSTNAME=$(hostname)
+echo "name of this machine is $HOSTNAME"
+
